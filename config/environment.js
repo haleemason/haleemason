4d8032458eb73081ad59292b6c424d3e2089140e
Ryan Mason <Rytiggy@gmail.com>
2017-09-22 10:57:52 -0400

    update mod pref

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   var ENV = {
-    modulePrefix:'',
+    modulePrefix:'ryanmason',
     environment: environment,
     rootURL: '/',
     locationType: 'auto',
