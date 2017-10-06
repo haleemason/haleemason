e6655c85de62956697d8502ac23e6ae3f9429bfe
Ryan Mason <Rytiggy@gmail.com>
2017-10-06 12:27:04 -0400

    ADD skills

--- app/components/component-gallery/component.js
+++ app/components/component-gallery/component.js
@@ -6,23 +6,23 @@ export default Ember.Component.extend({
 
 
 
-		$(document).ready(function(){
-			$('.entry').hover(function(){
-				var li = $('li', this);
+		// $(document).ready(function(){
+		// 	$('.entry').hover(function(){
+		// 		var li = $('li', this);
 				
-				for(var i = 0; i < $(li).length; i++){
-					$($(li)[i]).stop().animate({
-						'height':'18px'
-					}, 300 + (i * 150));
-				}
-			}, function(){
-				$('li', this).stop().animate({
-					'height':'5px'
-				}, 250);
-			});
+		// 		for(var i = 0; i < $(li).length; i++){
+		// 			$($(li)[i]).stop().animate({
+		// 				'height':'18px'
+		// 			}, 300 + (i * 150));
+		// 		}
+		// 	}, function(){
+		// 		$('li', this).stop().animate({
+		// 			'height':'5px'
+		// 		}, 250);
+		// 	});
 			
 			
-		});
+		// });
 		// ! function ($) {
 		// 	var defaults = {
 		// 		sectionContainer: "> section",
